b235045d4e610f871fd7c74b389d9c6a485bf95c
QiuMyodev <1040159471@qq.com>
2019-12-28 17:27:12 +0800

    增加后台自动更新天气的功能。

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.coolweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.coolweather.android.gson.Forecast;
 import com.coolweather.android.gson.Weather;
+import com.coolweather.android.service.AutoUpdateService;
 import com.coolweather.android.util.HttpUtil;
 import com.coolweather.android.util.Utility;
 
@@ -228,6 +230,12 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carwash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
-
+        
+        //激活AutoUpdateService服务
+        Intent intent =new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
+
+
+
 }
---
+++ app/src/main/java/com/coolweather/android/service/AutoUpdateService.java
@@ -0,0 +1,91 @@
+package com.coolweather.android.service;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+
+import com.coolweather.android.gson.Weather;
+import com.coolweather.android.util.HttpUtil;
+import com.coolweather.android.util.Utility;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+//后台自动更新天气
+public class AutoUpdateService extends Service {
+
+    public AutoUpdateService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    @Override
+    public int onStartCommand(Intent intent,int flags,int startId){
+        updateWeather();//更新天气
+        updateBingPic();//更新背景图片
+        AlarmManager manager=(AlarmManager) getSystemService (ALARM_SERVICE);
+        int anHour =8*60*60*1000;//这是8小时的毫秒数，为保证软件不会消耗过多流量，设置更新时间间隔为8小时
+        long triggerAtTime= SystemClock.elapsedRealtime()+anHour;
+        Intent i=new Intent(this,AutoUpdateService.class);
+        PendingIntent pi= PendingIntent.getService(this,0,i,0);
+        manager.cancel(pi);
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,triggerAtTime,pi);
+        return super.onStartCommand(intent,flags,startId);//8小时后，此方法会重新执行
+    }
+    //更新天气信息
+    private void updateWeather(){//更新后的数据直接存储到SharePreferences文件中
+        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
+        String weatherString=prefs.getString("weather",null);
+        if(weatherString !=null){
+            Weather weather= Utility.handleWeatherResponse(weatherString);
+            String weatherId=weather.basic.weatherId;
+
+            String weatherUrl="hettp://guolin.tech/api/weather?cityid="+weatherId+"&key=bc0418b57b2d4918819d3974ac1285d9";
+
+            HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                     String responseText=response.body().string();
+                    Weather weather=Utility.handleWeatherResponse(responseText);
+
+                    if(weather!=null&& "ok".equals(weather.status)){
+                        SharedPreferences.Editor editor=PreferenceManager.getDefaultSharedPreferences(AutoUpdateService.this).edit();
+                        editor.putString("weather",responseText);
+                        editor.apply();
+                    }
+                }
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
+    //更新必应每日一图
+    private void updateBingPic(){//更新后的数据直接存储到SharePreferences文件中
+        String requestBingPic ="http://guolin.tecg/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String bingPic=response.body().string();
+                SharedPreferences.Editor editor=PreferenceManager.getDefaultSharedPreferences(AutoUpdateService.this).edit();
+                editor.putString("bing_pic",bingPic);
+                editor.apply();
+            }
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
